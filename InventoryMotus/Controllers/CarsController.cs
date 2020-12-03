9cc0ef790ae8b15135fdb96c00b15a96a6208588
Alrick Visagie <Alrick.Visagie@Europcar.co.za>
2020-12-03 09:15:55 +0200

    Initial Commit

---
+++ InventoryMotus/Controllers/CarsController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace InventoryMotus.Controllers
+{
+
+    public class CarsController : Controller
+    {
+        //private object db;
+
+        public ActionResult Index()
+        {
+            return View();//db.Cars.ToList());
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult CreateCar(Models.Cars car)
+        {
+            //db.Cars.Add(car);
+            //db.SaveChanges();
+            return RedirectToAction("Index", "Cars");
+        }
+        [HttpPost]
+        public bool Delete(int id)
+        {
+            try
+            {
+                Models.Cars  car = db.Cars.Where(s => s.Id == id).First();
+                db.Cars.Remove(car);
+                db.SaveChanges();
+                return true;
+            }
+            catch (System.Exception)
+            {
+                return false;
+            }
+
+        }
+        public ActionResult Update(int id)
+        {
+            return View();// db.Cars.Where(s => s.Id == id).First());
+        }
+        [HttpPost]
+        public ActionResult UpdateCar(Models.Cars car)
+        {
+            Models.Cars ca = db.Cars.Where(s => s.Id == Cars.id).First();
+            ca.model = cars.model;
+            ca.make = cars.make;
+            ca.colour = cars.colour;
+            db.SaveChanges();
+            return RedirectToAction("Index", "Cars");
+
+
+        }
+
+    }
+}
---
+++ InventoryMotus/Models/Cars.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+
+namespace InventoryMotus.Models
+{
+    public class Cars
+    {
+        public int id { get; set; }
+        public string model { get; set; }
+        public string make { get; set; }
+        public string colour { get; set; }
+    }
+    
+}
